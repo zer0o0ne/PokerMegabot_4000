54025d88ef37941b4dee76eeb98cc3179d4aae62
Nikita <nikita2014semenov@gmail.com>
2022-11-04 17:51:49 +0300

    first baseline

--- poker/brain.py
+++ poker/brain.py
@@ -5,17 +5,21 @@ from modules import *
 
 
 class Agent(nn.Module):
-    def __init__(self, number, emb_args, MLP_arg, module_args, modules):
+    def __init__(self, number, emb_args = {}, start_args = {}, module_args = {}, modules = {}):
         super().__init__()
         self.number = number
-        self.start = nn.ModuleList([
-            Embedding(emb_args),
-            MLP(MLP_arg)
-        ])
-        self.modules = nn.ModuleDict({name: module(module_args[name]) for name, module in modules.items()})
+        self.start = nn.Sequential(
+            Embedding(**emb_args),
+            MLP(**start_args)
+        )
+        self.Modules = nn.ModuleDict({name: module(**module_args[name]) for name, module in modules.items()})
+        self.module_names = [name for name in modules]
 
     def forward(self, x):
         pass
+
+    def get_modules(self):
+        return self.module_names
     
 
 
@@ -26,19 +30,23 @@ class Brain:
         self.agents = [Agent(i, **agent_args) for i in range(self.num_agents)]
         self.modules = self.agents[0].get_modules()
         self.loss = loss
-        self.optimizers = [nn.optim.Adam(self.agents[i].parameters()) for i in range(self.num_agents)]
+        self.optimizers = [torch.optim.Adam(self.agents[i].parameters()) for i in range(self.num_agents)]
 
-    def sit(self, number):
-        self.players = np.random.choice(self.indicies, size = (number,), replace = False)
+    def sit(self, number, players = None):
+        if players is None:
+            self.players = np.random.choice(self.indicies, size = (number,), replace = False)
+        else:
+            self.players = players
 
     def step(self, position, env_state):
         start = [agent.start(env_state) for agent in self.agents]
         for module in self.modules:
-            start = [agent.modules[module](start) for agent in self.agents]
+            start = [agent.Modules[module](start) for agent in self.agents]
         
         return start[self.players[position]]
 
     def train(self, position, action, reward):
         loss = self.loss(action, reward)
-        loss.backward(inputs = self.agents[self.players[position]].parameters())
+        loss.backward(inputs = list(self.agents[self.players[position]].parameters()))
         self.optimizers[self.players[position]].step()
+        return loss.item()
